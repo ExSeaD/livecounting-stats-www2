c5556d43586e54064ad14b9fad0a9d5a13d10e71
rschaosid <rschaosid@gmail.com>
2014-08-01 23:25:19 -0600

    fix relative urls

--- public/services/StatsApi.js
+++ public/services/StatsApi.js
@@ -2,11 +2,11 @@ angular.module('livecounting-stats-www2')
 .factory('StatsApi', ['$http', function($http) {
   return {
     getKgets: function() {
-      return $http.get('/api/kgets.json')
+      return $http.get('api/kgets.json')
       .then(function(response) { return response.data; });
     },
     getContributors: function() {
-      return $http.get('/api/contributors.json')
+      return $http.get('api/contributors.json')
       .then(function(response) { return response.data; });
     }
   };
