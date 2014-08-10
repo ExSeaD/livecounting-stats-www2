61c8f83859751239040069edfdd5c8082d051095
rschaosid <rschaosid@gmail.com>
2014-08-09 22:20:18 -0600

    display punchcard

--- public/controllers/StatController.js
+++ public/controllers/StatController.js
@@ -2,4 +2,5 @@ angular.module('livecounting-stats-www2')
 .controller('StatController', ['$scope', 'StatsApi', function($scope, StatsApi) {
   StatsApi.getKgets().then(function(kgets) { $scope.kgets = kgets; });
   StatsApi.getContributors().then(function(contributors) { $scope.contributors = contributors; });
+  StatsApi.getPunchcard().then(function(punchcard) { $scope.punchcard = punchcard; });
 }]);
