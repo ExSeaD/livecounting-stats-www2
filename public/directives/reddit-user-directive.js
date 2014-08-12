e4b442ce86a0a408f4e3397e3cf6c7365d5dca71
rschaosid <rschaosid@gmail.com>
2014-08-11 23:35:04 -0600

    link styling
    * fade /u/ prefix
    * underline on hover
    * punchcard labels inherit colors

--- public/directives/reddit-user-directive.js
+++ public/directives/reddit-user-directive.js
@@ -5,6 +5,6 @@ angular.module('livecounting-stats-www2')
     scope: {
       'username': '=username'
     },
-    template: '<a href="https://www.reddit.com/u/{{username}}">/u/{{username}}</a>'
+    template: '<a href="https://www.reddit.com/u/{{username}}"><span class="username-prefix">/u/</span>{{username}}</a>'
   };
 }]);
