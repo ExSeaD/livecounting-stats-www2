15a60e1cb2d8263c69cc171d122b9313539880e1
rschaosid <rschaosid@gmail.com>
2014-08-12 22:13:22 -0600

    markup roughing

--- public/config.js
+++ public/config.js
@@ -1,24 +1,33 @@
 angular.module('livecounting-stats-www2')
 .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
   
-  $urlRouterProvider.otherwise('/gets');
+  $urlRouterProvider.otherwise('/overview');
   
-  $stateProvider.state('kgets', {
+  $stateProvider.state('overview', {
+    url: '/overview',
+    templateUrl: 'templates/overview.html'
+  });
+  
+  $stateProvider.state('detail', {
+    templateUrl: 'templates/detail.html'
+  })
+  
+  $stateProvider.state('detail.kgets', {
     url: '/gets',
     templateUrl: 'templates/kgets-page.html'
   });
   
-  $stateProvider.state('kgetters', {
+  $stateProvider.state('detail.kgetters', {
     url: '/getters',
     templateUrl: 'templates/kgetters-page.html'
   });
   
-  $stateProvider.state('participants', {
+  $stateProvider.state('detail.participants', {
     url: '/participants',
     templateUrl: 'templates/participants-page.html'
   });
   
-  $stateProvider.state('punchcard', {
+  $stateProvider.state('detail.punchcard', {
     url: '/activity',
     templateUrl: 'templates/punchcard-page.html'
   });
